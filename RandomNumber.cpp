d6a3296230275de6580406c98e16375faddf5c24
pjablons <01178530@pw.edu.pl>
2023-05-04 18:18:01 +0200

    RandomNumber improvements

--- RandomNumber.cpp
+++ RandomNumber.cpp
@@ -1,20 +1,29 @@
 #include "RandomNumber.h"
 
-RandomNumber::RandomNumber(const unsigned &seed, const long long &bot, const long long &top)
-{
+RandomNumber::RandomNumber(const unsigned &seed, const long long &bot, const long long &top) {
     std::uniform_int_distribution<long long> range(bot, top);
     std::mt19937 generator(seed);
     this->value = range(generator);
 }
 
-RandomNumber::RandomNumber(const unsigned &seed)
-{
+RandomNumber::RandomNumber(const long long &bot, const long long &top) {
+    RandomNumber(RandomNumber::RandomSeed(), bot, top);
+}
+
+RandomNumber::RandomNumber(const unsigned &seed) {
     std::uniform_int_distribution<unsigned> uid;
     std::mt19937 generator(seed);
     this->value = uid(generator);
 }
 
-const long long &RandomNumber::get_value() const noexcept
-{
+RandomNumber::RandomNumber() {
+    RandomNumber(RandomNumber::RandomSeed());
+}
+
+const long long &RandomNumber::get_value() const noexcept {
     return this->value;
+}
+
+unsigned RandomNumber::RandomSeed() {
+    return std::chrono::system_clock::now().time_since_epoch().count();
 }
--- RandomNumber.h
+++ RandomNumber.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <random>
+#include <chrono>
 
 class RandomNumber
 {
@@ -8,7 +9,10 @@ private:
 public:
     // Random unsinged long long
     RandomNumber(const unsigned &seed);
+    RandomNumber();
     // Random number in range
     RandomNumber(const unsigned &seed, const long long &bottom, const long long &top);
+    RandomNumber(const long long &bottom, const long long &top);
     const long long &get_value() const noexcept;
+    static unsigned RandomSeed();
 };
