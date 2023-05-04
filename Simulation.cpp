fa132a5c4cd2ed14e543d6e4c0093a9195cc7996
pjablons <01178530@pw.edu.pl>
2023-05-04 12:46:40 +0200

    Implemented prototype of handle_mod_table()

--- Simulation.cpp
+++ Simulation.cpp
@@ -1,12 +1,50 @@
 #include "Simulation.h"
 
+// Doesn't actually roll random Event at the start,
+// MUST start at NewTable, rolls after
 void Simulation::start() {
     for (; time != 0; --time) {
-        std::cout << get_curr_event_str() << std::endl;
+        //
+        std::cout << "Rolling random Event...\n";
+        //
+        sleep(200);
+        //
+        std::cout << "It's " << get_curr_event_str() << "!!\n";
+        handle_event(current_event);
         update_event();
-        sleep(100);
     }
     end();
+}
+
+void Simulation::handle_event(const Event &event) {
+    switch (event) {
+    case Event::ModTable:
+        handle_mod_table();
+        break;
+    }
+}
+
+void Simulation::handle_mod_table() {
+    for (Table &table : active_tables) {
+        if (!table.get_group().is_complete()) {
+            //
+            std::cout << group_at_table_str(table)
+                << "is not ready yet complete...\nBringing awaiting client no."
+                << table.get_awaiting_ids().front() << std::endl;
+            //
+            Client awaiting_c(table.get_awaiting_ids().front());
+            table.bring_to_table(awaiting_c);
+            table.update_status();
+        } else if (!table.ready_for_action()) {
+            //
+            std::cout << group_at_table_str(table)
+                << "has not received their orders yet\nPreparing...\n";
+            //
+            table.prepare_order();
+            table.update_status();
+        } else
+            table.interact(seed);
+    }
 }
 
 void Simulation::end() const {
@@ -50,4 +88,9 @@ Event Simulation::new_random_event() const noexcept {
     else if (r100 <= static_cast<unsigned short>(Event::DelTable)) return Event::NewTable;
     else if (r100 <= static_cast<unsigned short>(Event::KitchenAccident)) return Event::DelTable;
     else return Event::Nothing;
+}
+
+std::string Simulation::group_at_table_str(const Table &table) const noexcept {
+    return "The group no." + std::to_string(table.get_group().get_id())
+        + "at Table no." + std::to_string(table.get_id());
 }
--- Simulation.h
+++ Simulation.h
@@ -17,7 +17,7 @@
 // Chance of getting Event = [Event value] - [previous Event value]
 // Must cap at 100 and start at 0
 //
-// During ModTable, waiters attend to their tables
+// During ModTable, waiters attend to their tables or missing customers join the group
 // During Nothing, we wait for food to be prepared and/or eaten
 // During NewTable, new customers flood the scene
 // During DelTable, customers exit the building
@@ -31,14 +31,22 @@ class Simulation {
 private:
     size_t time = 0;
     unsigned seed;
-    std::vector<Table> tables;
-    Event current_event = Event::Nothing;
+    std::vector<Table> active_tables;
+    std::vector<Table> all_tables;
+    Event current_event = Event::NewTable;
 
     void update_seed();
     void update_event();
+    // Handlers
+
+    void handle_event(const Event &event);
+    void handle_mod_table();
     Event new_random_event() const noexcept;
     void sleep(const unsigned short& ms) const;
     void end() const;
+
+    // Printers
+    std::string group_at_table_str(const Table &table) const noexcept;
 
 public:
     Simulation(const size_t& time)
