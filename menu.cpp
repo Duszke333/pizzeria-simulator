5ec892ab2650463620e1af306adef9cf1bc94080
pwysocki <01178616@pw.edu.pl>
2023-05-04 16:22:41 +0200

    Unique pointers are no fun.

--- menu.cpp
+++ menu.cpp
@@ -22,26 +22,26 @@ void Menu::remove_appetizer(std::string the_name) {
     }));
 }
 
-void Menu::add_food(std::string the_name, unsigned int the_price, unsigned short prep_time) {
-    std::unique_ptr<Food> added_food = std::make_unique<Food>(the_name, the_price, prep_time);
-    foods.push_back(std::move(added_food));
-}
+// void Menu::add_food(std::string the_name, unsigned int the_price, unsigned short prep_time) {
+//     std::unique_ptr<Food> added_food = std::make_unique<Food>(the_name, the_price, prep_time);
+//     foods.push_back(std::move(added_food));
+// }
 
 void Menu::add_pizza(std::string the_name, unsigned int the_price, unsigned short prep_time, Size the_size) {
     std::unique_ptr<Pizza> added_pizza = std::make_unique<Pizza>(the_name, the_price, prep_time, the_size);
-    foods.push_back(std::move(added_pizza));
+    //foods.push_back(std::move(added_pizza));
     pizzas.push_back(Pizza(the_name, the_price, prep_time, the_size));
 }
 
 void Menu::add_drink(std::string the_name, unsigned int the_price, unsigned short prep_time, Volume the_vol) {
     std::unique_ptr<Drink> added_drink = std::make_unique<Drink>(the_name, the_price, prep_time, the_vol);
-    foods.push_back(std::move(added_drink));
+    //foods.push_back(std::move(added_drink));
     drinks.push_back(Drink(the_name, the_price, prep_time, the_vol));
 }
 
 void Menu::add_appetizer(std::string the_name, unsigned int the_price, unsigned short prep_time) {
     std::unique_ptr<Appetizer> added_appetizer = std::make_unique<Appetizer>(the_name, the_price, prep_time);
-    foods.push_back(std::move(added_appetizer));
+    //foods.push_back(std::move(added_appetizer));
     appetizers.push_back(Appetizer(the_name, the_price, prep_time));
 }
 
@@ -65,39 +65,42 @@ Pizza Menu::random_pizza(unsigned int seed) const {
 
 unsigned int Menu::calculate_total_price() const noexcept {
     unsigned int total = 0;
-    for (const auto& food_ptr : foods) total += food_ptr -> get_price();
+    //for (const auto& food_ptr : foods) total += food_ptr -> get_price();
+    for (const auto& drink : drinks) total += drink.get_price();
+    for (const auto& appetizer : appetizers) total += appetizer.get_price();
+    for (const auto& pizza : pizzas) total += pizza.get_price();
     return total;
 }
 
-Food Menu::find_by_name(std::string the_name) {
-    for (const auto& food_ptr : foods) {
-        if (food_ptr -> get_name() == the_name) return *food_ptr;
-    }
-    throw FoodNotFoundException(the_name);
-}
+// Food Menu::find_by_name(std::string the_name) {
+//     for (const auto& food_ptr : foods) {
+//         if (food_ptr -> get_name() == the_name) return *food_ptr;
+//     }
+//     throw FoodNotFoundException(the_name);
+// }
 
-void Menu::remove_by_name(std::string the_name) {
-    auto vector_eraser = foods.begin();
-    auto it = foods.begin();
-    while (it != foods.end()) {
-        if ((*it) -> get_name() == the_name) {
-            auto it_cp = it;
-            vector_eraser = it;
-            ++it;
-            foods.erase(it_cp);
-        }
-        else ++it;
-    }
-    if (dynamic_cast<Pizza*>((*vector_eraser).get())) {
-        remove_pizza(the_name);
-    }
-    else if (dynamic_cast<Drink*>((*vector_eraser).get())) {
-        remove_drink(the_name);
-    }
-    else if (dynamic_cast<Appetizer*>((*vector_eraser).get())) {
-        remove_appetizer(the_name);
-    }
-}
+// void Menu::remove_by_name(std::string the_name) {
+//     auto vector_eraser = foods.begin();
+//     auto it = foods.begin();
+//     while (it != foods.end()) {
+//         if ((*it) -> get_name() == the_name) {
+//             //auto it_cp = it;
+//             vector_eraser = it;
+//             ++it;
+//             //foods.erase(it_cp);
+//         }
+//         else ++it;
+//     }
+//     if (dynamic_cast<Pizza*>((*vector_eraser).get())) {
+//         remove_pizza(the_name);
+//     }
+//     else if (dynamic_cast<Drink*>((*vector_eraser).get())) {
+//         remove_drink(the_name);
+//     }
+//     else if (dynamic_cast<Appetizer*>((*vector_eraser).get())) {
+//         remove_appetizer(the_name);
+//     }
+// }
 
 std::ostream& operator<<(std::ostream& out, const Menu& menu) {
     out << "Menu contents: " << std::endl;
